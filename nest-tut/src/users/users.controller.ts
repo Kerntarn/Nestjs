010ab41dbbc41173d6005995ffa7d899a135890b
Kerntarn <fluk032548@gmail.com>
2024-06-06 16:58:50 +0700

    DTO, slightly validation

--- nest-tut/src/users/users.controller.ts
+++ nest-tut/src/users/users.controller.ts
@@ -1,24 +1,33 @@
-import { Controller, Get, Param, Body, Post} from '@nestjs/common';
+import { Controller, Get, Delete, Param, Body, Post, Query, Patch, ParseIntPipe} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
     @Get()
-    findAll(){
-        return []
-    }
-
-    @Get('interns')
-    findAllInterns(){
-        return ['all interns']
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+        return this.usersService.findAll(role)
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string){
-        return { id }
+    findOne(@Param('id', ParseIntPipe) id: number){
+        return this.usersService.findOne(id)       //+(Unary plus) can convert string to number
     }
 
     @Post()
-    create(@Body() user: {}){
-        return user
+    create(@Body() user: CreateUserDto){
+        return this.usersService.create(user)
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: UpdateUserDto){
+        return this.usersService.update(id, userUpdate)
+    }
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.usersService.delete(id)
     }
 }
--- nest-tut/src/users/users.service.ts
+++ nest-tut/src/users/users.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "A",
+            "role": "ADMIN"
+        },
+        {
+            "id": 2,
+            "name": "B",
+            "role": "ENGINEER"
+        },{
+            "id": 3,
+            "name": "C",
+            "role": "INTERN"
+        },
+        {
+            "id": 4,
+            "name": "D",
+            "role": "ADMIN"
+        }
+    ]
+
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+        if (role){
+            return this.users.filter(user => user.role === role)
+        } 
+        return this.users
+    }
+
+    findOne(id: number){
+        return this.users.find(user => user.id === id)
+    }
+
+    create( user: CreateUserDto){
+        const usersByHighestId = [...this.users].sort((a,b) => b.id - a.id)
+        const newUser = {
+            id : usersByHighestId[0].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id:number, updatedUser:UpdateUserDto){
+        this.users = this.users.map(user => {
+            if (user.id === id){
+                return {...user, ...updatedUser}    //updateUser's properties will overwrite existing user's properties
+            }
+            return user
+        })
+        return this.findOne(id)
+    }
+
+    delete(id:number){
+        const removedUser = this.findOne(id)
+        this.users = this.users.filter(user => user.id !== id)
+        return removedUser
+    }
+}
